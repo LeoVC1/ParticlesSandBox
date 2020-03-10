d12925360eb329bdd84db7b2bec8cdac4e80c87f
LeoVC <leovc_lll@outlook.com>
2020-03-10 13:51:54 -0300

    Game events

---
+++ ParticlesSandBox/Assets/GameEvents (1)/GameEventBoolListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventBoolListener : MonoBehaviour
+{
+    public GameEventBool Event;
+    public bool inverseParameter;
+    public BoolEvent Response;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised(bool value)
+    {
+        if(inverseParameter)
+            Response.Invoke(!value);
+        else
+            Response.Invoke(value);
+    }
+}
+
+[System.Serializable]
+public class BoolEvent : UnityEvent<bool> { }
